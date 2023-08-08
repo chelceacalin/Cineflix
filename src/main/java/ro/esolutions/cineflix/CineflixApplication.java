5afe723790337c29c16ec833eb5ff27fb0317ec5
andreea.vraciu <andreea.vraciu@esolutions.ro>
2023-08-08 10:38:03 +0000

    React create and refactoring

--- src/main/java/ro/esolutions/cineflix/CineflixApplication.java
+++ src/main/java/ro/esolutions/cineflix/CineflixApplication.java
@@ -1,13 +1,13 @@
-package com.example.demoInternship;
+package ro.esolutions.cineflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoInternshipApplication {
+public class CineflixApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoInternshipApplication.class, args);
+		SpringApplication.run(CineflixApplication.class, args);
 	}
 
 }
