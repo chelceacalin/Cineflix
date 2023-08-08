60cbd9d96d0c1cdc5c6202c298391818158d99f3
Calin <chelcea.calin@yahoo..com>
2023-08-08 15:53:23 +0300

    Added SVG icons and new routes

--- cineflix_front/src/App.jsx
+++ cineflix_front/src/App.jsx
@@ -1,24 +1,20 @@
-// App.js
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import MyProfile from './components/MyProfile/MyProfile'
 import Movies from './components/Movies/Movies'
-
+import CategoryManagement from "./components/CategoryManagement/CategoryManagement";
 
 function App() {
   return (
     <div className="app-container">
-
-    <Navbar />
-
+      <Navbar />
       <Router>
         <Routes>
-          
-        <Route index path="/" element={<Movies />} />
+          <Route index path="/" element={<Movies />} />
           <Route path="/myprofile/:id" element={<MyProfile />} />
+          <Route path="/categoryManagement" element={<CategoryManagement />} />
         </Routes>
-
       </Router>
     </div>
   );
--- cineflix_front/src/components/Movies/Movies.jsx
+++ cineflix_front/src/components/Movies/Movies.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
 
 function Movies() {
   return (
     <div className='ml-1'>
-
+        MOVIES
       
     </div>
   )
