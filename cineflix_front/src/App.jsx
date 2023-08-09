0b24bc924f3c50e88fe68635d70c2567ec59e59f
Calin <chelcea.calin@yahoo..com>
2023-08-09 08:59:22 +0300

    Navbar improved with svg and redirection functionality

--- cineflix_front/src/App.jsx
+++ cineflix_front/src/App.jsx
@@ -5,21 +5,39 @@ import MyProfile from './components/MyProfile/MyProfile'
 import Movies from './components/Movies/Movies'
 import CategoryManagement from "./components/CategoryManagement/CategoryManagement";
 import RoleManagement from "./components/RoleManagement/RoleManagement";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-  return (
+  return(
     <div className="app-container">
-      <Navbar />
       <Router>
+
+      <MainContent/>
+
+      </Router>
+
+    </div>
+
+  )
+
+
+  function MainContent(){
+
+  return (
+    <>
+      <Navbar />
         <Routes>
           <Route index path="/" element={<Movies />} />
           <Route path="/myprofile/:id" element={<MyProfile />} />
           <Route path="/categoryManagement" element={<CategoryManagement />} />
           <Route path="/roleManagement" element={<RoleManagement />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
-      </Router>
-    </div>
+    </>
+
   );
 }
+}
+
 
 export default App;
