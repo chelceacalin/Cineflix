02efffe4fb3f388d1137d63f3eaeffe051fa7287
Valentin Petier <valentin.petier@esolutions.ro>
2023-08-09 18:20:00 +0300

    added method in securityconfig

--- src/main/java/ro/esolutions/cineflix/config/SecurityConfig.java
+++ src/main/java/ro/esolutions/cineflix/config/SecurityConfig.java
@@ -2,19 +2,31 @@ package ro.esolutions.cineflix.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
-// To be deleted. Only for testing purposes.
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(http -> http.anyRequest().permitAll())
-                .build();
+        httpSecurity
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/admin/**")
+                        .hasRole("admin")
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .logout((logout)->logout.logoutUrl("/users/all"));
+
+        //httpSecurity.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
 
     }
 }
---
+++ src/main/resources/db/migration/V1_1__added_username.sql
@@ -0,0 +1,2 @@
+alter table user_cineflix drop column is_active;
+alter table user_cineflix add column username varchar(255);
