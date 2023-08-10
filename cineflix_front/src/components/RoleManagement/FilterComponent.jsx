502b66479d9be315c42fd600e082a0b63e9a25d6
Robert Popescu <robert.popescu@esolutions.ro>
2023-08-10 17:47:50 +0300

    Add sorting with dynamic direction and loading users from database and frontend table refactoring

--- cineflix_front/src/components/RoleManagement/FilterComponent.jsx
+++ cineflix_front/src/components/RoleManagement/FilterComponent.jsx
@@ -1,28 +1,36 @@
-import { Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material'
-import React from 'react'
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
+import React from "react";
 import "./css/FilterComponent.css";
-import { Button } from 'flowbite-react';
-
 
 function FilterComponent() {
-    return (
-        <>
-            <div className="filterContainer space-y-4 ml-6">
-                <div className="mt-10 mr-6">
-                    <TextField id="outlined-search" label="Search name" type="search" />
-                    </div>
-                <div className="mt-4 mr-6">
-                <TextField id="outlined-search" label="Search email" type="search" />
-                </div>
-                <div className="p-4">
-                    <FormGroup>
-                        <FormControlLabel control={<Checkbox />} label="Admin" />
-                        <FormControlLabel control={<Checkbox />} label="User" />
-                    </FormGroup>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="filterContainer space-y-4 ml-6">
+
+      <div className="mt-10 mr-6">
+        <label>First Name:</label>
+        <TextField id="outlined-search" label="Search first name" type="search" />
+      </div>
+      <div className="mt-10 mr-6">
+      <label>Last Name:</label>
+        <TextField id="outlined-search" label="Search last name" type="search" />
+      </div>
+      <div className="mt-4 mr-6">
+      <label>Email:</label>
+        <TextField id="outlined-search" label="Search email" type="search" />
+      </div>
+      <div className="p-4">
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label="Admin" />
+          <FormControlLabel control={<Checkbox />} label="User" />
+        </FormGroup>
+      </div>
+    </div>
+  );
 }
 
-export default FilterComponent
+export default FilterComponent;
