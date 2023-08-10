564f49dd59b42152f45aad11097b929110a42938
andreeav19 <andra_368@yahoo.com>
2023-08-10 10:28:49 +0300

    added logout handler

--- src/main/java/ro/esolutions/cineflix/config/SecurityConfig.java
+++ src/main/java/ro/esolutions/cineflix/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ro.esolutions.cineflix.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,26 +9,44 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/admin/**")
                         .hasRole("admin")
+                        .requestMatchers("/users/**")
+                        .hasAnyRole("admin", "user")
                         .anyRequest()
                         .authenticated()
                 )
                 .oauth2Login(Customizer.withDefaults())
-                .logout((logout)->logout.logoutUrl("/users/all"));
+                .logout((logout)->logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                );
 
         //httpSecurity.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return httpSecurity.build();
+    }
 
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
+
+        return oidcLogoutSuccessHandler;
     }
 }
