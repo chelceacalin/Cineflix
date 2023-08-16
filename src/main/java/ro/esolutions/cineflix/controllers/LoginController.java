7125b0367e90fa2224d0026e8f5eeeb6577d3e29
andreeav19 <andra_368@yahoo.com>
2023-08-16 15:54:45 +0300

    removed unused imports

--- src/main/java/ro/esolutions/cineflix/controllers/LoginController.java
+++ src/main/java/ro/esolutions/cineflix/controllers/LoginController.java
@@ -1,12 +1,9 @@
 package ro.esolutions.cineflix.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import ro.esolutions.cineflix.DTO.UserInfoDTO;
 import ro.esolutions.cineflix.services.UserCineflixService;
