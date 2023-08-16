e59ba4d7c7871d7d6da713fbd9277cfce2c4cc1f
Lazar Elena <elena.lazar1708@gmail.com>
2023-08-16 17:06:06 +0300

    createCategory endpoint

--- src/main/java/ro/esolutions/cineflix/exceptions/CategoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ro.esolutions.cineflix.exceptions;
-
-public class CategoryNotFoundException extends Exception{
-
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ro/esolutions/cineflix/exceptions/EmptyCategoryNameField.java
+++
@@ -1,8 +0,0 @@
-package ro.esolutions.cineflix.exceptions;
-
-public class EmptyCategoryNameField extends Exception{
-
-    public EmptyCategoryNameField(String message) {
-        super(message);
-    }
-}
