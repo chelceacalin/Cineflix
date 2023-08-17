4b1e6819fe7199b9b4db84170c34f97be14cca84
anca.lautarescu <anca.lautarescu@esolutions.ro>
2023-08-17 11:02:05 +0300

    added requests for sort and filter

--- cineflix_front/src/components/CategoryManagement/FilterCategory.jsx
+++ cineflix_front/src/components/CategoryManagement/FilterCategory.jsx
@@ -1,7 +1,14 @@
 import { TextField } from '@mui/material';
 import React from 'react';
+import { useEffect, useState } from "react";
 
-function FilterCategory() {
+function FilterCategory({ filterInput }) {
+  let [name, setName] = useState("");
+  useEffect(() => {
+    let array = [];
+    array.push(name);
+    filterInput(array);
+  }, [name]);
   return (
     <div className="w-72 ml-6 border-r-2">
       <div className="mt-10 mr-6">
@@ -12,7 +19,7 @@ function FilterCategory() {
           id="outlined-search"
           name="name"
           type="search"
-        //   onChange={(e) => setFirstName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
     </div>
