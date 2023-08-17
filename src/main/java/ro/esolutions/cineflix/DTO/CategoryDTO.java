82243a5a487e03205b864a2d9e56cb2da859e50c
anca.lautarescu <anca.lautarescu@esolutions.ro>
2023-08-17 15:20:37 +0300

    pagination and added id for category in dto

--- src/main/java/ro/esolutions/cineflix/DTO/CategoryDTO.java
+++ src/main/java/ro/esolutions/cineflix/DTO/CategoryDTO.java
@@ -5,11 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDTO {
+
+    private UUID id;
 
     private String name;
 }
