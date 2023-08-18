e25be5f3feb080562d558f7e309886312fb83000
Robert Popescu <robert.popescu@esolutions.ro>
2023-08-18 13:07:42 +0300

    Modified the url variable to work with rentedBy

---
+++ cineflix_front/src/components/MyProfile/Movie.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import Button from '@mui/material/Button';
+
+import DetailsMovieModalView from "./DetailsMovieModalView";
+import DeleteMovieModalView from "./DeleteMovieModalView";
+
+function Movie({ title, director, category, isAvailable, rentedUntil, rentedBy, rating, classes, updateMovie }) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+  return (
+    <tr key={title}>
+      <td className={classes}>
+        <div variant="small" color="blue-gray" className="font-normal">
+          {title}
+        </div>
+      </td>
+      <td className={classes}>
+        <div variant="small" color="blue-gray" className="font-normal">
+          {director}
+        </div>
+      </td>
+      <td className={classes}>
+        <div variant="small" color="blue-gray" className="font-normal">
+          {category}
+        </div>
+      </td>
+      <td className={classes}>
+        <div variant="small" style={{color: isAvailable? 'green' : 'red'}} className="font-normal">
+          {isAvailable? "Available" : "Unavailable"}
+        </div>
+      </td>
+      <td className={classes}>
+        <div variant="small" color="blue-gray" className="font-normal">
+          {!rentedUntil ? "N/A" : rentedUntil}
+        </div>
+      </td>
+      <td className={classes}>
+        <div variant="small" color="blue-gray" className="font-normal">
+          {rentedBy&&isAvailable ? "" : rentedBy}
+        </div>
+      </td>
+      <td className={classes}>
+        <div>
+            <button onClick={handleOpen} className="font-normal bg-white hover:border-hover-cream hover:text-hover-cream text-blue-marine border border-blue-marine py-2 px-3 mr-3">
+                Details
+            </button>
+          {/* <DetailsMovieModalView
+            isModalOpen={open}
+            closeModal={handleClose}
+            firstName={firstName}
+            lastName={lastName}
+            name={name}
+            role={role}
+            email={email}
+            username={username}
+            updateMovie={updateMovie}
+          /> */}
+            <button onClick={handleOpen} className="font-normal bg-blue-marine hover:border-hover-cream hover:bg-hover-cream text-white border border-blue-marine py-2 px-3">
+                Delete
+            </button>
+          { <DeleteMovieModalView
+            isModalOpen={open}
+            closeModal={handleClose}
+            title={title}
+            movieId={""}
+            deleteMovie={""}
+          /> }        
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+export default Movie;
---
+++ cineflix_front/src/components/MyProfile/MyProfileFilterComponent.jsx
@@ -0,0 +1,138 @@
+import {
+    Checkbox,
+    FormControlLabel,
+    FormGroup,
+    TextField,
+    InputLabel,
+    NativeSelect
+  } from "@mui/material";
+  import React from "react";
+  import "./css/MyProfileFilterComponent.css";
+  import "react-datepicker/dist/react-datepicker.css";
+  import { useEffect, useState } from "react";
+  import DatePicker from "react-datepicker";
+  
+  function MyProfileFilterComponent({ filterInput }) {
+    let [title, setTitle] = useState("");
+    let [director, setDirector] = useState("");
+    let [category, setCategory] = useState("");
+    let [available, setAvailable] = useState(true);
+    let [unavailable, setUnavailable] = useState(true);
+    let [rentedUntil, setRentedUntil] = useState(new Date());
+    let [rentedBy, setRentedBy] = useState("");
+  
+    useEffect(() => {
+      let date = `${rentedUntil.getFullYear()}-${(rentedUntil.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${rentedUntil.getDate().toString().padStart(2, "0")}`;
+      let array = [];
+      if (
+        (available == true && unavailable == true) ||
+        (available == false && unavailable == false)
+      ) {
+        array.push(category, director, title, "BOTH", date, rentedBy);
+      } else if (available == true && unavailable == false) {
+        array.push(category, director, title, "AVAILABLE", date, rentedBy);
+      } else if (available == false && unavailable == true) {
+        array.push(category, director, title, "UNAVAILABLE", date, rentedBy);
+      } else array.push(category, director, title, "", date, rentedBy);
+      filterInput(array);
+    }, [
+      category,
+      director,
+      title,
+      available,
+      unavailable,
+      rentedUntil,
+      rentedBy,
+    ]);
+  
+    return (
+      <div className="filterContainer space-y-4 ml-6">
+        <div className="mt-4 mr-6">
+          <TextField
+            id="outlined-search"
+            name="title"
+            label="Search title"
+            type="search"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="mt-10 mr-6">
+          <TextField
+            id="outlined-search"
+            name="director"
+            label="Search director"
+            type="search"
+            onChange={(e) => setDirector(e.target.value)}
+          />
+        </div>
+        <div className="mt-10 mr-6">
+          <TextField
+            id="outlined-search"
+            name="category"
+            label="Search category"
+            type="search"
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </div>
+        <div className="p-1">
+          <div className="mb-2">Availability: </div>
+          <div className="flex gap-2">
+            <div>
+              <label name="unavailable">Unavailable</label>
+              <Checkbox
+                name="type"
+                label="Unavailable"
+                defaultChecked
+                onClick={(e) => {
+                  setUnavailable(e.target.checked);
+                }}
+              />
+            </div>
+            <div>
+              <label name="available">Available</label>
+              <Checkbox
+                name="type"
+                label="Available"
+                defaultChecked
+                onClick={(e) => {
+                  setAvailable(e.target.checked);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 mr-6">
+          <label>Rented Until:</label>
+          <DatePicker
+            selected={rentedUntil}
+            onChange={(date) => setRentedUntil(date)}
+          />
+        </div>
+        <div className="mt-4 mr-6">
+          {/* <TextField
+              id="outlined-search"
+              name="title"
+              label="Search rented by"
+              type="search"
+              onChange={(e) => setRentedBy(e.target.value)}
+            /> */}
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+              Rented By
+            </InputLabel>
+            <NativeSelect
+              defaultValue={"username"}
+              onChange={(e) => ""}
+              placeholder=""
+            >
+              <option value="USER">User1</option>
+              <option value="ADMIN">User2</option>
+            </NativeSelect>
+        </div>
+      </div>
+    );
+  }
+  
+  export default MyProfileFilterComponent;
+  
