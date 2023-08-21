b97b124186f362283fbca0c1770b92ed04e1f0a3
Robert Popescu <robert.popescu@esolutions.ro>
2023-08-21 18:01:49 +0300

    Adding partially working view for delete movies

--- cineflix_front/src/components/MyProfile/DeleteMovieModalView.jsx
+++ cineflix_front/src/components/MyProfile/DeleteMovieModalView.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Button, Dialog, DialogContent, FormControl, InputLabel, NativeSelect, TextField } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import "./css/AddNewMovieModalWindow.css";
+import "./css/DeleteMovieModalView.css";
 import axios from 'axios';
 
 axios.defaults.withCredentials = true
@@ -12,39 +12,26 @@ function DeleteMovieModalView({ isModalOpen, closeModal, title, movieId, deleteM
 
 
     const confirmToDelete = () => {
-        let url = '/movies/delete/' + selectedOption;
-
-        try {
-            setUserDTO(() => ({
-                'username': username,
-                'firstName': firstName,
-                'lastName': lastName,
-                'email': email,
-                'role': selectedOption
-            }));
-            const response = axios.post(url, userDTO).then(()=>{
-
-                updateUser(userDTO);
-                closeModal();
-            });
-        } catch (error) {
-        }
+        
     };
 
     return (
         <Dialog open={isModalOpen} onClose={closeModal}>
-            <FontAwesomeIcon className="closeModalWindowButton" icon={faTimes} onClick={closeModal} />
-            <DialogContent>
-                <div className='mt-6'>
-                    Are you sure you want to permanently delete this entry?
+            <div className="overflow-x-hidden">
+                <FontAwesomeIcon className="closeModalWindowButton" icon={faTimes} onClick={closeModal} />
+                <DialogContent>
+                <div className="w-64 break-normal text-center">
+                    <p> Are you sure you want to permanently delete this entry?</p>
                 </div>
-                <div className='mt-4'>
-                    <FormControl fullWidth>
-                        <Button className="save" variant="contained" onClick={confirmToDelete}>Confirm</Button>
-                        <Button className="cancel" variant="outlined" onClick={closeModal} >Cancel</Button>
-                    </FormControl>
+                
+                <div className="mt-2 mb-2">
+                    <Button className="contained-button w-full" variant="contained" onClick={deleteMovie}>Yes</Button>
                 </div>
-            </DialogContent>
+                <div className="mb-2">
+                    <Button className="outlined-button w-full" variant="outlined" onClick={closeModal} >Cancel</Button>
+                </div>
+                </DialogContent>
+            </div>            
         </Dialog>
     );
 }
