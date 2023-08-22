7fd7a3226c54e9a40b8de757e5c40e150df2485a
Calin <chelcea.calin@yahoo..com>
2023-08-22 15:18:09 +0300

    finalized front end design

--- cineflix_front/src/components/Movies/Movies.jsx
+++ cineflix_front/src/components/Movies/Movies.jsx
@@ -1,13 +1,22 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import Pagination from "../RoleManagement/Pagination";
-import RentedMovie from './RentedMovie'
+import RentedMovie from "./RentedMovie";
 import { UserLoginContext } from "../../utils/context/LoginProvider";
-
-
+import MovieFilter from "./MovieFilter";
 function Movies() {
-  const TABLE_HEAD = ["Title", "Director", "Category", "Status","Rented On","Rented Until", "Rented By","","  "];
+  const TABLE_HEAD = [
+    "Title",
+    "Director",
+    "Category",
+    "Status",
+    "Rented On",
+    "Rented Until",
+    "Rented By",
+    "",
+    "  ",
+  ];
   const [movies, setMovies] = useState([]);
   const [category, setCategory] = useState("");
   const [director, setDirector] = useState("");
@@ -15,24 +24,21 @@ function Movies() {
   const [isAvailable, setIsAvailable] = useState("");
   const [rentedUntil, setRentedUntil] = useState("");
   const [rentedBy, setRentedBy] = useState("");
-  const [rentedOn,setRentedOn]=useState("")
-  let [ownerUsername, setOwnerUsername] = useState("");
-  const [sortField, setSortField] = useState("title");
-  const [lastClicked, setLastClicked] = useState(null);
+  const [rentedDate, setrentedDate] = useState("");
   let [newUrl, setNewUrl] = useState("");
   let [pageNo, setPageNo] = useState(1);
   let [pageSize, setPageSize] = useState(15);
   let [totalPages, setTotalPages] = useState("");
   let [totalMovies, setTotalMovies] = useState(0);
-  let [addMovieUrl, setAddMovieUrl] = useState("");
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [triggerRefresh, setTriggerRefresh] = useState(false);
   const { username } = useContext(UserLoginContext);
-
   const [direction, setDirection] = useState(true);
-
+  const [sortField, setSortField] = useState("title");
+  let [ownerUsername, setOwnerUsername] = useState("");
+  let [lastClicked,setLastClicked]=useState("")
 
   let handleClick = (fieldName) => {
     if (lastClicked === fieldName) {
@@ -42,14 +48,34 @@ function Movies() {
   };
 
   useEffect(() => {
-    const normalizedSortField = sortField || "title";
-
-    newUrl = `/movies?owner_username=${username}&sortField=${normalizedSortField}&direction=${
-        direction ? "ASC" : "DESC"}&title=${title}&director=${director}&category=${category}&isAvailable=${isAvailable}&pageNo=${
-            parseInt(pageNo) - 1}&pageSize=${pageSize}`;
-
-            console.log(newUrl)
-    axios.get(newUrl).then((elems) => {
+    const buildUrl = () => {
+      const normalizedSortField = sortField || "title";
+      let params = [
+        `owner_username=${username}`,
+        `sortField=${normalizedSortField}`,
+        `direction=${direction ? "ASC" : "DESC"}`,
+        `title=${title}`,
+        `director=${director}`,
+        `category=${category}`,
+        `isAvailable=${isAvailable}`,
+        `pageNo=${parseInt(pageNo) - 1}`,
+        `pageSize=${pageSize}`,
+      ];
+  
+      if (rentedUntil) {
+        params.push(`rentedUntil=${rentedUntil}`);
+      }
+  
+      if (rentedBy) {
+        params.push(`rentedBy=${rentedBy}`);
+      }
+  
+      return `/movies?${params.join("&")}`;
+    };
+  
+    const url = buildUrl();
+  
+    axios.get(url).then((elems) => {
       if (elems.data.content.length === 0 && pageNo > 1) {
         updatePageNumber(pageNo - 1);
       } else {
@@ -57,8 +83,22 @@ function Movies() {
         setTotalPages(elems.data.totalPages);
       }
     });
-  }, [triggerRefresh, sortField, direction, title, director, category, isAvailable, rentedUntil, rentedBy, ownerUsername, pageSize, pageNo, movies.length ]);
-
+  }, [
+    triggerRefresh,
+    sortField,
+    direction,
+    title,
+    director,
+    category,
+    isAvailable,
+    rentedUntil,
+    rentedBy,
+    ownerUsername,
+    pageSize,
+    rentedDate,
+    pageNo,
+    movies.length,
+  ]); 
   let getFilterInput = (params) => {
     setCategory(params[0]);
     setDirector(params[1]);
@@ -77,66 +117,11 @@ function Movies() {
     setPageNo(pgNo);
   };
 
-  const addMovie = (addedMovie) => {
-    const addedMovies = movies.map(movie => {
-      if (movie.title === addedMovie.title) {
-        return addedMovie;
-      }
-      return movie;
-    });
-    setMovies(updatedMovie);
-  };
-
-  let handleDirectionChange = (e) => {
-    const columnContent = e.target.textContent;
-  
-    switch (columnContent) {
-      case "Title": {
-        setSortField("title");
-        setDirection(!direction);
-        break;
-      }
-      case "Director": {
-        if (title.length > 0 || director.length > 0 || category.length > 0) {
-          setDirection(!direction);
-        }
-        setSortField("director");
-        handleClick(columnContent.toLowerCase());
-        break;
-      }
-      case "Category": {
-        if (title.length > 0 || director.length > 0 || category.length > 0) {
-          setDirection(!direction);
-        }
-        setSortField("category");
-        handleClick(columnContent.toLowerCase());
-        break;
-      }
-      case "Rented Until": {
-        if (title.length > 0 || director.length > 0 || category.length > 0) {
-          setDirection(!direction);
-        }
-        setSortField("rentedUntil");
-        handleClick(columnContent.toLowerCase());
-        break;
-      }
-      case "Rented By": {
-        if (title.length > 0 || director.length > 0 || category.length > 0) {
-          setDirection(!direction);
-        }
-        setSortField("rentedBy");
-        handleClick(columnContent.toLowerCase());
-        break;
-      }
-      default:
-        break;
-    }
-  };
-  
 
   return (
     <>
-    <div className="w-full h-full ml-10 mr-10 mt-5">
+    <MovieFilter filterInput={getFilterInput}/>
+      <div className="w-full h-full ml-10 mr-10 mt-5">
         <table className="w-full min-w-max table-auto text-left">
           <thead className="bg-basic-red text-white">
             <tr>
@@ -144,16 +129,36 @@ function Movies() {
                 return (
                   <th
                     key={elem}
-                    className={`}border-b-white p-4 ${elem.length>2?'hover':''} cursor-pointer`}
+                    className={`border-b-white p-4 ${
+                      elem.length > 2 ? "hover" : ""
+                    } cursor-pointer`}
                     onClick={(e) => {
                       e.preventDefault();
-
                       if (e.target.textContent !== "Status") {
-                            handleDirectionChange()
+                        if (e.target.textContent == "Title") {
+                          setSortField("title");
+                        } else if (e.target.textContent === "Director") {
+                          setSortField("director");
+                        } else if (e.target.textContent === "Category") {
+                          setSortField("category");
+                        }
+                        if (sortField == e.target.textContent.toLowerCase()) {
+                          setDirection(!direction);
+                        } else {
+                          setDirection(true);
+                        }
+
+                        if (e.target.textContent == "Rented Until") {
+                          setSortField("rentedUntil");
+                          setDirection(!direction);
+                        } else if (e.target.textContent === "Rented By") {
+                          setSortField("rentedBy");
+                          setDirection(!direction);
+                        }
                       }
                     }}
                   >
-                    <div className="">
+                   <div className="">
                       {elem}
                       <svg
                         data-column={elem}
@@ -164,7 +169,8 @@ function Movies() {
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                         onClick={(e) => {
-                          setDirection(!direction);
+                          e.stopPropagation();
+                          setDirection(!direction)
                           handleClick(e.currentTarget.getAttribute("data-column").toLowerCase());
                         }}
                       >
@@ -179,40 +185,51 @@ function Movies() {
                   </th>
                 );
               })}
-            
             </tr>
           </thead>
           <tbody className="text-blue-marine">
-          {movies.map(({ category, director, title, isAvailable, rentedUntil, rentedBy, id, rentedDate}, index) => {
-              const isLast = index === movies.length - 1;
-              const classes = isLast
-                ? "px-4 py-2"
-                : "px-4 py-2 border-b border-blue-gray-50";
+            {movies.map(
+              (
+                {
+                  category,
+                  director,
+                  title,
+                  isAvailable,
+                  rentedUntil,
+                  rentedBy,
+                  id,
+                  rentedDate,
+                },
+                index
+              ) => {
+                const isLast = index === movies.length - 1;
+                const classes = isLast
+                  ? "px-4 py-2"
+                  : "px-4 py-2 border-b border-blue-gray-50";
 
-              return (
-                <RentedMovie
-                  id={id}
-                  title={title}
-                  category={category}
-                  director={director}                  
-                  isAvailable={isAvailable}
-                  rentedUntil={rentedUntil}
-                  rentedOn={rentedDate}
-                  rentedBy={rentedBy}
-                  key={index}
-                  classes={classes}
-                  triggerRefresh={triggerRefresh}
-                  setTriggerRefresh={setTriggerRefresh}
+                return (
+                  <RentedMovie
+                    id={id}
+                    title={title}
+                    category={category}
+                    director={director}
+                    isAvailable={isAvailable}
+                    rentedUntil={rentedUntil}
+                    rentedDate={rentedDate}
+                    rentedBy={rentedBy}
+                    key={index}
+                    classes={classes}
+                    triggerRefresh={triggerRefresh}
+                    setTriggerRefresh={setTriggerRefresh}
                   />
-              );
-            })}
+                );
+              }
+            )}
           </tbody>
         </table>
         <span className="w-full bg-basic-red flex flex-wrap py-3 mb-4">
           <span className=" inline-flex marginResizable">
-            <p className="text-white font-normal">
-              Results per page:{" "}
-            </p>
+            <p className="text-white font-normal">Results per page: </p>
             <p className="ml-5">
               <select
                 name="cars"
@@ -240,7 +257,7 @@ function Movies() {
         </span>
       </div>
     </>
-  )
+  );
 }
 
 export default Movies;
