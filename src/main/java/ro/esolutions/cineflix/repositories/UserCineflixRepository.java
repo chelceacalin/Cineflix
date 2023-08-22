a986b22ffcad877cdc5a237791919e50fefa62c5
Valentin Petier <valentin.petier@esolutions.ro>
2023-08-22 10:33:00 +0300

    removed unused imports

--- src/main/java/ro/esolutions/cineflix/repositories/UserCineflixRepository.java
+++ src/main/java/ro/esolutions/cineflix/repositories/UserCineflixRepository.java
@@ -2,12 +2,10 @@ package ro.esolutions.cineflix.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.esolutions.cineflix.entities.UserCineflix;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface UserCineflixRepository extends JpaRepository<UserCineflix, String>, JpaSpecificationExecutor<UserCineflix> {
