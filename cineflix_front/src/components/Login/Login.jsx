c4fcb0fc5b341c0901fc2fac1b7233f5d0e4b512
andreeav19 <andra_368@yahoo.com>
2023-08-22 10:32:01 +0300

    added mage and background color

--- cineflix_front/src/components/Login/Login.jsx
+++ cineflix_front/src/components/Login/Login.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 function Login() {
 
+  useEffect(() => {
+    document.body.style.background="#C64432";
+
+    return () => {
+      document.body.style.background="white";
+    };
+  }, []);
+
   return (
-    <>
-      <div>Login</div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          window.location.href = "http://localhost:8081";
-        }}
-      >
-        Go to login
-      </button>
-    </>
+      <div className="login-bg" style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        maxWidth: "100%",
+        maxHeight: "100%",
+      }}>
+        <img
+          src="./Images/LoginImage.png"
+          alt="Login Image"
+        />
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "http://localhost:8081";
+          }}
+        >
+          Go to login
+        </button>
+      </div>   
+
   );
 }
 
