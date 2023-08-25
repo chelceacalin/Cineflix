a170cc4c5851b3ffffef2f820db51e41c39c4de4
dorin.chitu <dorin.chitu@esolutions.ro>
2023-08-25 07:11:47 +0000

    Revert "Update cors mappings"
    This reverts commit 2857bf6b84290b3dc489f6a8286a99ef5bf3b3f7

--- src/main/java/ro/esolutions/cineflix/config/CorsConfig.java
+++ src/main/java/ro/esolutions/cineflix/config/CorsConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("*")
+                    .allowedOrigins(frontendAppUrl)
                     .allowedHeaders("*")
                     .allowCredentials(true);
         }
